7fa963850d7d8adb7d788cc2d616b33c465d552c
Peterson Portella <109779768+PetersonPortella@users.noreply.github.com>
2024-04-27 22:52:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/*let nome = 'peterson';
+nome = 'lis';*/
+
+const nome = 'peterson'; //colocar const e let antes do console.log, no caso as variaveis antes 
+
+console.log(nome," vai acordar e ir para o trabalho");
+console.log("depois", nome, " vai almoçar");
+console.log(nome,"joga bola");
+console.log(nome,"vai dormir");
+console.log(nome,"começa outro dia tudo de novo");
+
